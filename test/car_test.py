e359515c64f68d5fc47e50f929c2896993c69fa1
project of class <kasraabdi2024@gmail.com>
2025-08-03 04:57:14 +0330

    change

--- test/car_test.py
+++ test/car_test.py
@@ -1,7 +1,14 @@
 from model.entity.car import Car
 from model.repository.car_repository import CarRepository
+from model.repository.database_creator import create_connection
 
-car = Car(100, "Benz", "cls", "Black", "2022", "50000$", 0)
+create_connection()
+
+car = Car(100, "benz", "cls", "Black", "2022", "50000$", 0)
 
 repo = CarRepository()
+repo.delete(car.code)
 repo.save(car)
+
+print(car)
+print("Car test executed successfully.")
--- test/customer_test.py
+++ test/customer_test.py
@@ -1,7 +1,25 @@
 from model.repository.customer_repository import CustomerRepository
 from model.entity.customer import Customer
+from model.repository.database_creator import create_connection
 
+# اتصال به پایگاه داده را ایجاد می‌کنیم
+create_connection()
+
+# یک شیء مشتری با کد و نام کاربری ثابت ایجاد می‌کنیم
 customer = Customer(200, "ali", "bahrami", "ALI123", "ali345", "09193647381", 0)
 
+# یک نمونه از repository ایجاد می‌کنیم
 repo = CustomerRepository()
+
+# ابتدا رکورد مشتری را بر اساس نام کاربری پیدا می‌کنیم
+existing_customer = repo.find_by_username(customer.username)
+
+# اگر رکورد از قبل وجود داشت، آن را حذف می‌کنیم
+if existing_customer:
+    repo.delete(existing_customer[0])  # existing_customer[0] is the code
+
+# حالا می‌توانیم رکورد جدید را بدون مشکل ذخیره کنیم
 repo.save(customer)
+
+print(customer)
+print("Customer test executed successfully.")
